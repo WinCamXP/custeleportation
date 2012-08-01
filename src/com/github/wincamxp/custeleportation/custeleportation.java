8466dd00049b7fc4bf8685936423adbb0dd36a47
WinCamXP <l33tg33k@live.com>
2012-07-31 21:23:24 -0500

    v1.1 Release
    Added permission system for direct teleport.

--- src/com/github/wincamxp/custeleportation/custeleportation.java
+++ src/com/github/wincamxp/custeleportation/custeleportation.java
@@ -9,11 +9,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class custeleportation extends JavaPlugin {
 
 	public void onEnable(){
-		getLogger().info("Custeleportation 1.0 initialized.");
+		getLogger().info("Custeleportation 1.1 initialized.");
 	}
 	
 	public void onDisable(){
-		getLogger().info("Custeleportation 1.0 disabled.");
+		getLogger().info("Custeleportation 1.1 disabled.");
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
@@ -26,20 +26,28 @@ public class custeleportation extends JavaPlugin {
 				}else if (tpto == null){
 					return false;
 				}else{
-					Location loc = tpto.getLocation();
+					if (sender.isOp()){
+						Location loc = tpto.getLocation();
 					tper.teleport(loc);
 					return true;
+					}else{
+						return false;
+					}
 				}
 			}else if (args.length == 1){
 				Player tpto = (Bukkit.getServer().getPlayer(args[0]));
 				if (tpto == null){
 					return false;
 				}else{
-					if ((sender instanceof Player)){
-						Location loc = tpto.getLocation();
-					Player p = (Player) sender;
-					p.teleport(loc);
-					return true;
+					if (sender.hasPermission("cust.tp." + args[0]) || sender.isOp()){
+						if ((sender instanceof Player)){
+							Location loc = tpto.getLocation();
+						Player p = (Player) sender;
+						p.teleport(loc);
+						return true;
+						}
+					}else{
+						return false;
 					}
 				}
 			}else{
